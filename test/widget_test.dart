2f8639bf45fe316c8d754a4bc67c3735c2e8b9c7
Eatlotsofpie1 <roger.tram@yahoo.com>
2024-04-29 22:33:44 -0700

    added initial widget finding tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:prox_spot/main.dart';
+import 'package:prox_spot/pages/add_location_form.dart';
+import 'package:prox_spot/pages/edit_favorite_page.dart';
+import 'package:prox_spot/pages/favorite_list_page.dart';
+import 'package:prox_spot/pages/home_page.dart';
+import 'package:prox_spot/pages/location_item.dart';
+import 'package:prox_spot/pages/location_list.dart';
+import 'package:prox_spot/pages/map_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget( MyApp());
+  testWidgets('Find Widgets', (WidgetTester tester) async {
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(MyApp());
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    expect(find.byIcon(Icons.login), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+
+    await tester.pumpWidget(const FavoriteListPage());
+
+    expect(find.byIcon(Icons.add), findsOneWidget);
+    expect(find.text('Favorite Locations'), findsOneWidget);
+
   });
 }
