902b7141563135a6176020805c23044c42907b40
summersCodingLog <summerxia1105@gmail.com>
2024-05-13 18:48:42 -0700

    add coverage

---
+++ test/full_coverage_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: unused_import
+import 'package:prox_spot/main.dart';
+import 'package:prox_spot/pages/add_location_form.dart';
+import 'package:prox_spot/pages/edit_favorite_page.dart';
+import 'package:prox_spot/pages/favorite_list_page.dart';
+import 'package:prox_spot/pages/home_page.dart';
+import 'package:prox_spot/pages/location_item.dart';
+import 'package:prox_spot/pages/location_list.dart';
+import 'package:prox_spot/pages/map_page.dart';
+
+void main() {}
